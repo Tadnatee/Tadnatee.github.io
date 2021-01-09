8706486d7cee07a5ed60031f5c57792b8127957f
Tadnatee <76645147+Tadnatee@users.noreply.github.com>
2021-01-09 23:09:03 +0700

    added slide show

---
+++ js/gallery.js
@@ -0,0 +1,8 @@
+<script language="JavaScript" src="{{ "/js/gallery.js" }}"></script>
+<script type="text/javascript">
+$(document).ready(function(){
+	$('.gallery').slick({
+	setting-name: setting-value
+						});
+							});
+</script>
