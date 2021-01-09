a7b55776d5af04ba4be967c0c85f8996d1bf7867
Tadnatee <76645147+Tadnatee@users.noreply.github.com>
2021-01-10 00:08:18 +0700

    readded slick, changed setting js

--- js/gallery.js
+++ js/gallery.js
@@ -1,8 +1,46 @@
 <script language="JavaScript" src="{{ "/js/gallery.js" }}"></script>
 <script type="text/javascript">
 $(document).ready(function(){
-	$('.gallery').slick({
-	setting-name: setting-value
-						});
-							});
+$(document).ready(function(){
+ $('.gallery').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        nfinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        nfinite: true,
+        dots: true
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+ });
+
+});
 </script>
